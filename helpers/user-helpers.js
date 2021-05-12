6746542ddff1657def942f0a34153d16dee84e90
Sivin Saji <sivinsaji22@gmail.com>
2021-05-12 15:12:03 +0530

    __UserSignup, Bcrypt (signup:-- Here we collect name,email,password and this data is stored into our database 'shopping' in collection 'user'. Here we use bcrypt library to store our password in an id format for security. And after the successful signup user is redirected into HomePage(/).)

---
+++ helpers/user-helpers.js
@@ -0,0 +1,17 @@
+var db=require('../config/connection')
+//var collection=require('../config/collections')
+const bcrypt = require("bcrypt");
+const collections = require('../config/collections');
+module.exports={
+    doSignup: (userData) => {
+        return new Promise(async (resolve, reject) => {
+          userData.password = await bcrypt.hash(userData.password, 10);
+          db.get()
+            .collection(collections.USER_COLLECTION)
+            .insertOne(userData)
+            .then((data) => {
+              resolve(data.ops[0]);
+            });
+        });
+      }
+}
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var productHelpers=require('../helpers/product-helpers')
+const userHelpers=require('../helpers/user-helpers')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   
   productHelpers.getAllProducts().then((products)=>{
-    res.render('user/view-products',{admin:true,products});       
+    res.render('user/view-products',{products});       
   })
 });
-
+router.get("/login", function (req, res) {
+  res.render('user/login')
+})
+router.get("/signup", function (req, res) {
+  res.render("user/signup");  
+})
+router.post("/signup", function (req, res) {
+  
+  userHelpers.doSignup(req.body).then((response) => {
+    console.log(response)
+  })
+})
 module.exports = router;
