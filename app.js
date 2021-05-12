3fbe6f56447f06979abcce79fe150da459939664
Sivin Saji <sivinsaji22@gmail.com>
2021-05-12 20:56:03 +0530

    SESSION  (Session:-- Here we install express-session to create session is used to store the data of user and set time limit to session. When we use session in app.js we create 2 argumments one secret key and cookie. Here we set 10 minutes to timeout the session.Next when session exist we change the account button into user name if session doesnot exist the button will be account it self. When we logout then the session will be destroyed.)

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session=require('express-session')
+var hbs=require('express-handlebars');
+var app = express();
 
 
 var userRouter = require('./routes/user');
 var adminRouter = require('./routes/admin');
-var hbs=require('express-handlebars');
-var app = express();
+
 var fileUpload=require('express-fileupload')
 var db=require('./config/connection')
+
 
 
 // view engine setup
@@ -25,6 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload())
+app.use(session({secret:"key",cookie:{maxAge:600000}}))
 
 db.connect((err)=>{
   if(err) console.log('Connection Error'+err)
--- routes/user.js
+++ routes/user.js
@@ -4,9 +4,10 @@ var productHelpers=require('../helpers/product-helpers')
 const userHelpers=require('../helpers/user-helpers')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  
+  let user=req.session.user
+  console.log(user)
   productHelpers.getAllProducts().then((products)=>{
-    res.render('user/view-products',{products});       
+    res.render('user/view-products',{products,user});       
   })
 });
 router.get("/login", function (req, res) {
@@ -24,10 +25,17 @@ router.post("/signup", function (req, res) {
 router.post('/login' ,(req,res)=>{
   userHelpers.doLogin(req.body).then((response)=>{
     if(response.status){
+      req.session.loggedIn=true
+      req.session.user=response.user
+
     res.redirect('/')
     }else{
       res.redirect('/login')
     }
   })
+})
+router.get('/logout',(req,res)=>{
+  req.session.destroy()
+  res.redirect('/')
 })
 module.exports = router;
