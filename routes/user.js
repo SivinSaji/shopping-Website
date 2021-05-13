b7fcb33f27e93cc9b79d9cf7067230a8847e2857
Sivin Saji <sivinsaji22@gmail.com>
2021-05-13 12:24:06 +0530

    Cart   (link for cart button created, We set a Middleware for check thus user logedIn or not. When user click the cart button with logedIn then it will display a dummy cart page otherwise it will be redirected into login page

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 var productHelpers=require('../helpers/product-helpers')
 const userHelpers=require('../helpers/user-helpers')
+const verifyLogin=(req,res,next)=>{
+  if(req.session.loggedIn){
+    next()
+  }else{
+     res.redirect('/login')
+  }
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let user=req.session.user
@@ -47,5 +54,8 @@ router.post('/login' ,(req,res)=>{
 router.get('/logout',(req,res)=>{
   req.session.destroy()
   res.redirect('/')
+})
+router.get('/cart',verifyLogin,(req,res)=>{
+  res.render('user/cart')
 })
 module.exports = router;
