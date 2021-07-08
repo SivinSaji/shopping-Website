f92c7069f7be7047c576d27e2b355fe7bea9192d
Sivin Saji <sivinsaji22@gmail.com>
2021-07-08 19:17:56 +0530

    Order Page and Order product page updated

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload())
-app.use(session({secret:"key",cookie:{maxAge:600000}}))
+app.use(session({secret:"key",cookie:{maxAge:60000000}}))
 
 db.connect((err)=>{
   if(err) console.log('Connection Error'+err)
